267a984660e3fea490100c0043cbd2b2a752571f
abc <abc@example.com>
2024-01-25 01:31:51 +0530

    React with vite

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,21 @@
+
+
+import Chai from "./Chai"
+
+
+function App() {
+  const username = "Sarup";
+
+  return (
+    <>
+      <Chai />
+      <h1>chai aur react {username}</h1>
+      <p>test paragraph</p>
+
+    </>
+  )
+}
+
+export default App
+
+
---
+++ 01vitereact/src/Chai.jsx
@@ -0,0 +1,11 @@
+function Chai() {
+    return (
+        <>
+            <h3>chai is ready</h3>
+            <h1>Chai aur react with vite | SM</h1>
+            <p>test paragraph</p>
+        </>
+    )
+}
+
+export default Chai
