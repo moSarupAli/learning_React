6ba49e86f9407c1bcca5368ff88eecd4c817242c
abc <abc@example.com>
2024-01-25 01:34:27 +0530

    React counter - small project

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,65 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  let [counter, setCounter] = useState(15)
+
+  // let counter = 15;
+  /* 
+    const addValue = () => {
+      // console.log("clicked", Math.random());
+      // console.log("clicked", counter);
+      // counter = counter + 1;
+      // setCounter(counter + 1);
+  
+      if (counter < 20) // Home Work
+        setCounter(counter + 1);
+    }
+  
+  
+    const removeValue = () => {
+      // setCounter(counter - 1);
+  
+      if (counter > 0) // Home Work
+        setCounter(counter - 1);
+    }
+   */
+
+  // A react interview question on counter
+  const addValue = () => {
+    setCounter(prevCounter => prevCounter + 1);
+    setCounter(prevCounter => prevCounter + 1);
+    setCounter(prevCounter => prevCounter + 1);
+    setCounter(prevCounter => prevCounter + 1);
+    setCounter(prevCounter => prevCounter + 1);
+  }
+
+  const removeValue = () => {
+    // setCounter(counter - 1);
+
+    setCounter(prevCounter => prevCounter - 1);
+    setCounter(prevCounter => prevCounter - 1);
+    setCounter(prevCounter => prevCounter - 1);
+    setCounter(prevCounter => prevCounter - 1);
+    setCounter(prevCounter => prevCounter - 1);
+  }
+
+  return (
+    <>
+      <h1>Chai aur React</h1>
+      <h2>Counter value: {counter}</h2>
+
+      <button
+        onClick={addValue}>Add value {counter}</button>
+      <br />
+      <button
+        onClick={removeValue}>remove value {counter}</button>
+      <p>footer: {counter}</p>
+    </>
+  )
+}
+
+export default App
